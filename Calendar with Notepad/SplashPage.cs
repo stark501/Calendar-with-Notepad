8d20c9416500d848d26b7d1265e34de714595f8d
Radoslav304 <radoslavcabadaj304@gmail.com>
2022-03-18 15:11:07 +0100

    Add project files.

---
+++ Calendar with Notepad/SplashPage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Reflection.Emit;
+using Xamarin.Forms;
+
+
+namespace Calendar_with_Notepad
+{
+    public class SplashPage : ContentPage
+    {
+        Image splashImage;
+
+        public SplashPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            var sub = new AbsoluteLayout();
+            splashImage = new Image
+            {
+                Source = "calendar.jpg",
+                WidthRequest = 200,
+                HeightRequest = 200
+
+            };
+            AbsoluteLayout.SetLayoutFlags(splashImage,
+                AbsoluteLayoutFlags.PositionProportional);
+            AbsoluteLayout.SetLayoutBounds(splashImage,
+             new Rectangle(0.5, 0.5, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+
+            sub.Children.Add(splashImage);
+
+            this.BackgroundColor = Color.FromHex("#fff");
+            this.Content = sub;
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await splashImage.ScaleTo(1, 2000);
+            await splashImage.ScaleTo(0.5, 800, Easing.Linear);
+
+            Application.Current.MainPage = new NavigationPage(new Page1());
+        }
+    }
+}
